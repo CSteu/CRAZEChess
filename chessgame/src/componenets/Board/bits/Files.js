0983f88824d62ec9aaf4f6f4f0adba0d9675e4f8
Chase Steuerwald <Chase.Steuerwald@trojans.dsu.edu>
2023-09-29 20:39:40 -0500

    added logo and deleted react app

--- chessgame/src/componenets/Board/bits/Files.js
+++
@@ -1,9 +0,0 @@
-import './Files.css'
-
-const Files = ({files}) => {
-    return <div className="files">
-        {files.map(file => <span key={file}>{file}</span>)}
-    </div>
-}
-
-export default Files
